9b4aa4d1bb9298074c3ffe1b64d6d5837a18bb78
VaporyPower72 <signupalex19@gmail.com>
2020-07-01 22:50:57 +0200

    Added reset button

--- src/components/LikeCounter.js
+++ src/components/LikeCounter.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"; // <- note the added import of useState
 
 export default function LikeCounter() {
   const initial_numLikes = 0;
   const [numLikes, set_numLikes] = useState(initial_numLikes); // <- using state!
 
   const increment = () => {
-    console.log("Yes, clicked! Current number of likes:", numLikes);
+    console.log("Yes, clicked! current number of likes", numLikes);
     set_numLikes(numLikes + 1);
+  };
+
+  const toZero = () => {
+    set_numLikes(numLikes - numLikes);
   };
 
   return (
     <div>
       <p>
         This post has <b>{numLikes}</b> likes!
-        <button onClick={increment}>Like</button>
+        <button className="btn btn-primary" onClick={increment}>
+          Like
+        </button>
+        <button onClick={toZero}>Reset</button>
       </p>
     </div>
   );
