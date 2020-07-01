cc2cd055207aa5bf2f32a06a7758bcee21f89c26
VaporyPower72 <signupalex19@gmail.com>
2020-07-01 23:56:26 +0200

    Added article list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import LikeCounter from "./components/LikeCounter";
 import LikeButton from "./components/LikeButton";
 import AwesomeAnimals from "./components/AwesomeAnimals";
+import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <LikeCounter />
       <LikeButton />
       <AwesomeAnimals />
+      <ArticleList />
     </main>
   );
 }
